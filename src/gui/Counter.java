4618d9586f23f1d1b94e3262ace7aab39f955186
1020510 <1020510@QHS-A220-14.quincypublicschools.com>
2018-06-12 14:02:07 -0400

    Gui package

--- src/gui/Counter.java
+++ src/gui/Counter.java
@@ -1,5 +1,38 @@
 package gui;
 
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
 public class Counter {
 
+	private int countLives;
+	private int countBeans;
+	private JLabel beans;
+	private JLabel lives;
+	
+	public Counter(int beansNum, int livesNum) {
+		countBeans = beansNum;
+		countLives = livesNum;
+		beans = new JLabel("Beans - " + countBeans);
+		lives = new JLabel("Lives - " + countLives);
+	}
+	
+	public void addCount(int count) {
+		count+=1;		
+	}
+	public void downCount(int count) {
+		count-=1;		
+	}
+	
+	
+	
+	
 }
