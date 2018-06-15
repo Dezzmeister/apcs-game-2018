6db7ce9b8d7ff672ca2bc36be3dd900372f873ae
1022352 <1022352@QHS-A220-15.quincypublicschools.com>
2018-06-15 13:31:05 -0400

    items

--- src/image/Actor.java
+++ src/image/Actor.java
@@ -1,5 +1,32 @@
 package image;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
-public class Actor extends Entity {
+public class Actor implements Entity {
+	
+		private BufferedImage enemies = null;
+		
+		public BufferedImage getImage() {
+		
+		try {
+		   
+		   enemies = ImageIO.read(getClass().getResource("E:/choichirlpix.png"));
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		return enemies;
+		
+	}
+		public void draw() {
+	        drawImage(enemies);
+		}
+		
 
+		private void drawImage(BufferedImage image2) {
+			// TODO Auto-generated method stub
+			
+		}
 }
--- src/image/Entity.java
+++ src/image/Entity.java
@@ -1,5 +1,12 @@
 package image;
 
-public class Entity {
+import java.awt.image.BufferedImage;
 
+public interface Entity{
+    
+        public BufferedImage getImage();
+            //defined in adopted children (classes that implement this, since every img will have a different location [name] ; they don’t share any variables/constructors)
+        
 }
+
+
