61198bb73ff861f6fa7654e504e10eca1bea7aae
Dan Hagborg <daniel.hagborg@gmail.com>
2018-06-18 14:05:04 -0400

    audio stuff

---
+++ src/audio/MusicPlayer.java
@@ -0,0 +1,38 @@
+package audio;
+
+import javax.sound.midi.*;
+
+/**
+ * Used with Song for playing music (.mid)
+ * @author Dan Hagborg //not Austin
+ * @version 1.0
+ * @since 2018-6-18
+ */
+public class MusicPlayer {
+	private Sequencer player;
+	public MusicPlayer() {
+		try{
+			player = MidiSystem.getSequencer();
+		}catch (MidiUnavailableException e) {
+			player = null;
+			e.printStackTrace();
+		}
+	}
+	public void setSong(Song s){
+		stop();
+		try{
+			player.setSequence(s.getSequence());
+			player.open();
+			player.setLoopCount(Sequencer.LOOP_CONTINUOUSLY);
+			player.start();
+		}catch(Exception e){
+			e.printStackTrace();
+		} 
+		
+	}
+	public void stop(){
+		if (player.isRunning()) player.stop();
+	}
+	public static MusicPlayer m = new MusicPlayer();
+
+}
