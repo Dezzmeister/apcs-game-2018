2dd2e68dba08cc03fb28eed48f2fb37728a2cf28
Dezz Meister <dezzy3359@gmail.com>
2018-11-22 11:01:18 -0500

    healthkits oscillate

--- src/main/entities/Healthkit.java
+++ src/main/entities/Healthkit.java
@@ -11,6 +11,11 @@ public class Healthkit extends Pickup {
 	private static final Model HEALTHKIT_MODEL;
 	public float cylinderRadiusSquared;
 	private static final float BASE_PSI = 0.017f;
+	private static final float MAX_ADDED_HEIGHT = 0.17f;
+	private static final float MIN_ADDED_HEIGHT = 0.05f;
+	private float addedHeight = 0;
+	private static final float SINE_PERIOD = 2;
+	private float sineClock = 0;
 	
 	static {		
 		float hKitScaleF = 0.35f;
@@ -28,14 +33,30 @@ public class Healthkit extends Pickup {
 		cylinderRadiusSquared = boundary.radius * boundary.radius;
 	}
 	
-	private static final Matrix4 TRANSLATE_IN = Transformer.createTranslationMatrix(-0.5f, -0.5f, 0);
-	private static final Matrix4 TRANSLATE_OUT = TRANSLATE_IN.inverse();
+	private static final float SINE_B = (float) ((2 * Math.PI) / SINE_PERIOD);
+	private static final float SINE_AMP = MAX_ADDED_HEIGHT - MIN_ADDED_HEIGHT;
+	private static final float SINE_VSHIFT = (MAX_ADDED_HEIGHT - MIN_ADDED_HEIGHT) / 2;
 	
 	@Override
 	public void animateFrame(double delta) {
+		updateSineClock(delta);
+		
+		Matrix4 TRANSLATE_IN = Transformer.createTranslationMatrix(-0.5f, -0.5f, -addedHeight);
+		
+		addedHeight = (float) (SINE_AMP * Math.sin(SINE_B * sineClock) + SINE_VSHIFT);
+		
+		Matrix4 TRANSLATE_OUT = Transformer.createTranslationMatrix(0.5f, 0.5f, addedHeight);
+		
 		Matrix4 rotator = Transformer.createZRotationMatrix(BASE_PSI * (float)delta);
 		
 		Matrix4 transformer = TRANSLATE_IN.multiply(rotator).multiply(TRANSLATE_OUT);
 		model.transformAndApply(transformer);
-	}	
+	}
+	
+	private static final float SINE_ADJUSTER = 0.01f;
+	
+	private void updateSineClock(double delta) {
+		sineClock += delta * SINE_ADJUSTER;
+		sineClock %= SINE_PERIOD;
+	}
 }
